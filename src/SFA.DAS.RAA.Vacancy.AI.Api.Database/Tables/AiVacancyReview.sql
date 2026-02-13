7f93f1b49fdff5458326bd8b63446f2c6e5d003f
killij <51908793+killij@users.noreply.github.com>
2026-02-13 10:40:45 +0000

    FAI-3001 Update table def

--- src/SFA.DAS.RAA.Vacancy.AI.Api.Database/Tables/AiVacancyReview.sql
+++ src/SFA.DAS.RAA.Vacancy.AI.Api.Database/Tables/AiVacancyReview.sql
@@ -4,6 +4,7 @@ CREATE TABLE dbo.AiVacancyReview (
     [Status]            nvarchar(12) NOT NULL,
     [Output]            nvarchar(max) NULL,
     [CreatedDate]       datetime2(7) NOT NULL DEFAULT GETDATE(),
+    [UpdatedDate]       datetime2(7) NULL,
     CONSTRAINT [PK_AiVacancyReview] PRIMARY KEY (VacancyReviewId),
     INDEX [IX_AiVacancyReview_VacancyId] NONCLUSTERED(VacancyId)
 )
