8651b0c2229a4a0af092978669f937f1334a04ce
killij <51908793+killij@users.noreply.github.com>
2026-03-06 16:38:30 +0000

    FAI-3173 Exclude nsb infra from code coverage

--- src/SFA.DAS.RAA.Vacancy.AI.Api/Core/NServiceBus/NServiceBusServiceProviderFactory.cs
+++ src/SFA.DAS.RAA.Vacancy.AI.Api/Core/NServiceBus/NServiceBusServiceProviderFactory.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using NServiceBus.ObjectBuilder.MSDependencyInjection;
 
 namespace SFA.DAS.RAA.Vacancy.AI.Api.Core.NServiceBus;
 
+[ExcludeFromCodeCoverage]
 public class NServiceBusServiceProviderFactory : IServiceProviderFactory<UpdateableServiceProvider>
 {
     public UpdateableServiceProvider CreateBuilder(IServiceCollection services)
